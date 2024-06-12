78a4ce595a09853c3932f88383a6b9d81c755a73
sola1010nuo <jenny38178843@gmail.com>
2024-06-12 22:51:41 +0800

    修改一下

--- pages/api/videos.ts
+++ pages/api/videos.ts
@@ -32,7 +32,45 @@ function uploadVideoStream(req: NextApiRequest, res: NextApiResponse) {
   return;
 }
 
+const CHUNK_SIZE_IN_BYTES = 1000000; // 1 mb
 
+function getVideoStream(req: NextApiRequest, res: NextApiResponse) {
+  const range = req.headers.range;
+
+  if (!range) {
+    return res.status(400).send("Rang must be provided");
+  }
+
+  const videoId = req.query.videoId;
+
+  const videoPath = `./videos/${videoId}.mp4`;
+
+  const videoSizeInBytes = fs.statSync(videoPath).size;
+
+  const chunkStart = Number(range.replace(/\D/g, ""));
+
+  const chunkEnd = Math.min(
+    chunkStart + CHUNK_SIZE_IN_BYTES,
+    videoSizeInBytes - 1
+  );
+
+  const contentLength = chunkEnd - chunkStart + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${chunkStart}-${chunkEnd}/${videoSizeInBytes}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  res.writeHead(206, headers);
+  const videoStream = fs.createReadStream(videoPath, {
+    start: chunkStart,
+    end: chunkEnd,
+  });
+
+  videoStream.pipe(res);
+}
 
 function getVideoFiles(req: NextApiRequest, res: NextApiResponse) {
   const videosFolderPath = path.join(process.cwd(), 'videos');
@@ -59,6 +97,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return getVideoFiles(req, res);
   }
   
+  if (method === "GET") {
+    return getVideoStream(req, res);
+  }
 
   if (method === "POST") {
     return uploadVideoStream(req, res);
